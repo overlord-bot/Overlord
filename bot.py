ac6e9d215c7a55d6d979e480ccd9220d3cdff48b
jackwang9988 <39379864+jwgit9@users.noreply.github.com>
2022-09-11 12:20:11 -0400

    Minor functionality changes
    - added bot status
    - added bot description
    - added a few reaction abilities

--- bot.py
+++ bot.py
@@ -8,25 +8,34 @@ from dotenv import load_dotenv  # pip install python-dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')  # fetches token from env file stored locally
 
+bot_status = "With Fate | Try !help "
+bot_description = "This is the full help description"
+
 intents = discord.Intents.default()
+intents.members = True  # permission to see server members
 intents.message_content = True  # permission to read message content
 
-client = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', description=bot_description, intents=intents, activity=discord.Game(name=bot_status))
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    print(message.content)
-    if message.author == client.user:
+    if message.author == bot.user:
         return
+    elif message.content.startswith("hi"):
+        await message.channel.send('hello')  # reacts with message in the location it was sent from
+    elif message.content == 'msg':
+        await message.author.send('👋')  # sends a direct message to the user
+    elif message.content == 'react':
+        await message.add_reaction('👍')  # adds a emoji reaction to a message, press windows key + '.' to bring up emoji list
+        await message.add_reaction('❤')
+        await message.add_reaction('🆗')
 
-    if message.content == 'hi':
-        await message.channel.send('hello')
+    await bot.process_commands(message)  # https://discordpy.readthedocs.io/en/stable/faq.html#why-does-on-message-make-my-commands-stop-working
 
-
-client.run(TOKEN)
+bot.run(TOKEN)
