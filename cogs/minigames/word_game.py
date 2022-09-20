e82fa9ed266804104848d215483728ad78cafb9f
Eagle1456 <amadeaus02@gmail.com>
2022-09-20 17:20:31 -0400

    Add command to wordgame

--- cogs/minigames/word_game.py
+++ cogs/minigames/word_game.py
@@ -1,4 +1,18 @@
 # Clone of the game wordle
 
+from discord.ext import commands
+
 class WordGame(commands.Cog, name="Word Game"):
-    pass
+    """Plays the word game that is similar to Wordle"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.word = "Jeans"
+
+    @commands.command()
+    async def getword(ctx, arg):
+        """Returns the test word. FOR DEBUG ONLY!"""
+        await ctx.send("Jeans")
+
+async def setup(bot):
+    await bot.add_cog(WordGame(bot))
