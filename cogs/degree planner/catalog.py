f99bf31e423b323cc034b09795f8385e97753243
Alan Zhang <eggyrepublic8@gmail.com>
2022-09-20 17:08:03 -0400

    test commit on branch

---
+++ cogs/degree planner/catalog.py
@@ -0,0 +1,17 @@
+from array import *
+
+# Categories to collect data on:
+#
+#   communication intensive
+#   HASS pathway
+#   concentration
+#   core class
+#
+# Categories to check for graduation requirements:
+#
+#   math electives
+#   science electives
+#   
+# Other features:
+#
+#   similarity graph
