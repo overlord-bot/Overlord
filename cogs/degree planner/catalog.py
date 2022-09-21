c32edeaff96ece182d787bc4f77a486f198c0d38
EggyRepublic-WS <eggyrepublic@gmail.com>
2022-09-21 01:55:13 -0400

    testing bot running

---
+++ cogs/degree planner/catalog.py
@@ -0,0 +1,15 @@
+from array import *
+
+class Catalog:
+
+    #catalog will  be a list of courses and degrees
+    #also store graphs for further analysis and course prediction of free electives
+
+    courses = []
+    degrees = []
+
+    def add_course(course):
+        courses.append(course)
+
+    def add_degree(degree):
+        degrees.append(degree)
--- cogs/degree planner/schedule.py
+++ cogs/degree planner/schedule.py
@@ -1,5 +1,24 @@
 from array import *
+from discord.ext import commands
+from course import *
 
-class Schedule:
+class Schedule(commands.Cog, name="Degree Planner"):
+
+    def __init__(self, bot):
+        self.bot = bot
 
     master_list = []
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user or message.author.bot:
+            return
+        elif message.content.startswith("create course"):
+            await message.channel.send("generating a course")
+            course = Course()
+            course.name = "data structures"
+            await message.channel.send("created " + course.name)
+
+
+async def setup(bot):
+    await bot.add_cog(Schedule(bot))
