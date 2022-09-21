8b3bd80ed3cb92a0b72eca522cf90ed9dd076bfe
jwgit9 <39379864+jwgit9@users.noreply.github.com>
2022-09-21 19:26:44 -0400

    Add ranking and popularity

--- cogs/webcrawling/anime_scraper.py
+++ cogs/webcrawling/anime_scraper.py
@@ -27,8 +27,10 @@ class AnimeScraper(commands.Cog, name="Anime Scraper"):
         anime_soup = BeautifulSoup(anime_data.text, 'html.parser')
         score = anime_soup.find("div", {"class": "score-label"}).get_text()
         description = anime_soup.find("p", {"itemprop": "description"}).get_text()
+        ranking = anime_soup.find("span", {"class": "numbers ranked"}).get_text()
+        popularity = anime_soup.find("span", {"class": "numbers popularity"}).get_text()
 
-        await context.send(f"URL: {anime_url} | Score: {score} | Description: {description}")
+        await context.send(f"URL: {anime_url} | Score: {score}/10 | {ranking} | {popularity} \n\nDescription: \n{description}")
 
 
 async def setup(bot):
