acdb0e93a9462928e55c0767ba439586c6b34133
whitefieldcat <whitefieldcat@gmail.com>
2022-09-21 16:11:41 -0400

    Restructure basic_math cog
    - Remove placeholder commands
    - Add helper class

--- cogs/math/basic_math.py
+++ cogs/math/basic_math.py
@@ -2,22 +2,53 @@
 
 from discord.ext import commands
 
+import math
+
+class PostfixOperator:
+    """Helper class for stack-based postfix operators"""
+
+    def __init__(self, func, arg_count):
+        self.func = func # a function that accepts and modifies a stack in place
+        self.arg_count = arg_count # exact number of required arguments
+
+    def __call__(self, stack):
+        if len(stack) < self.arg_count:
+            raise Exception("insufficient number of arguments on stack")
+
+        result = self.func(stack)
+        del stack[-self.arg_count:] # pop arg_count values off
+        stack.append(result)
+
+OPERATORS = {
+    "neg": PostfixOperator(lambda stack: -stack[-1], 1),
+    "+": PostfixOperator(lambda stack: stack[-2] + stack[-1], 2),
+    "-": PostfixOperator(lambda stack: stack[-2] - stack[-1], 2),
+    "*": PostfixOperator(lambda stack: stack[-2] * stack[-1], 2),
+    "/": PostfixOperator(lambda stack: stack[-2] / stack[-1], 2),
+    "^": PostfixOperator(lambda stack: math.pow(stack[-2], stack[-1]), 2),
+    "sqrt": PostfixOperator(lambda stack: math.sqrt(stack[-1]), 1)
+}
+
+def reverse_polish(expression):
+    tokens = expression.split()
+    stack = []
+
+    for token in tokens:
+        if token in OPERATORS:
+            OPERATORS[token](stack)
+        else: # assume anything other than an operator is a value
+            stack.append(float(token))
+
+    if len(stack) == 1:
+        return stack[0]
+    else:
+        raise Exception("extraneous values remaining on the stack")
 
 class BasicMath(commands.Cog, name="Basic Math"):
     """Calculates basic math"""
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def add(self, context, left: int, right: int):
-        """Adds two numbers"""  # this is the description that will show up in !help
-        await context.send(left + right)
-
-    @commands.command()
-    async def subtract(self, context, left: int, right: int):
-        """Subtracts first number from the second"""  # this is the description that will show up in !help
-        await context.send(left - right)
 
     @commands.group()
     async def calc(self, context):
@@ -27,42 +58,10 @@ class BasicMath(commands.Cog, name="Basic Math"):
 
     @calc.command(name="rpn")
     async def calc_rpn(self, context, *, expression):
-        tokens = expression.split()
-        stack = []
-
         try:
-            for token in tokens:
-                if token == "+":
-                    b = stack.pop()
-                    a = stack.pop()
-                    stack.append(a + b)
-                elif token == "-":
-                    b = stack.pop()
-                    a = stack.pop()
-                    stack.append(a - b)
-                elif token == "*":
-                    b = stack.pop()
-                    a = stack.pop()
-                    stack.append(a * b)
-                elif token == "/":
-                    divisor = stack.pop()
-                    dividend = stack.pop()
-
-                    if divisor == 0:
-                        await context.reply("Division by zero")
-                        return
-
-                    stack.append(dividend / dividend)
-                elif token == "^":
-                    exponent = stack.pop()
-                    base = stack.pop()
-                    stack.append(base ** exponent)
-                else: # assume token is a number by default for now
-                    stack.append(float(token))
-
-            await context.reply(stack[0])
+            await context.reply(reverse_polish(expression))
         except Exception as e:
-            await context.reply("Error:", e)
+            await context.reply(f"Error: {e}")
 
 async def setup(bot):
     await bot.add_cog(BasicMath(bot))
