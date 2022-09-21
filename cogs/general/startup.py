9623b6ef00910351c6cae279adc7c47488e7ac5f
jwgit9 <39379864+jwgit9@users.noreply.github.com>
2022-09-21 00:35:49 -0400

    Update startup.py

--- cogs/general/startup.py
+++ cogs/general/startup.py
@@ -22,7 +22,10 @@ class Startup(commands.Cog, name="Start Up"):
         print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
 
         await self.bot.wait_until_ready()
-        if (self.bot.testing_server):
+
+        # Syncs bot commands with the specific bot testing server in bot.py
+        # If a server id is not provided, new commands added may take an hour or more to sync with discord
+        if self.bot.testing_server:
             self.bot.tree.copy_global_to(guild=self.bot.testing_server)
             await self.bot.tree.sync(guild=self.bot.testing_server)
             print(f"Syncing commands to server id: {self.bot.testing_server.id}")
