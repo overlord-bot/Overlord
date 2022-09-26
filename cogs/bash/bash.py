87e1e54d014f834c3987b52354aefcc1a5b5a541
VNA818-RPI <Arcerv@rpi.edu>
2022-09-26 13:16:29 -0400

    Added comments

--- cogs/bash/bash.py
+++ cogs/bash/bash.py
@@ -2,8 +2,13 @@ import discord
 import asyncio
 from discord.ext import commands
 from discord import app_commands
+#from cogs.bash.util.BashView import BashView
 
 import docker
+'''
+pip install pywin32==225
+pip install docker
+'''
 
 class Bash(commands.GroupCog, name="bash"):
 	"""
@@ -11,15 +16,19 @@ class Bash(commands.GroupCog, name="bash"):
 	"""
 	global testing_server
 
+	#Connect to Docker api @tcp://localhost:2375
 	client = docker.from_env()
 
 	def __init__(self, bot: commands.bot):
 		self.bot = bot
 
 	async def run(self, command) -> str:
+		#Build the image from the Dockerfile, might take a while if first time running
 		built = self.client.images.build(path="./cogs/bash", tag="disc:v1")
+		#Create a container from the newly created image and run the command
 		ret = self.client.containers.run("disc:v1", command=["/bin/sh", "-c", command], remove=True)
 		print(self.client.containers.list())
+		#Decode and return result
 		return str(ret.decode("utf-8"))
 
 	@app_commands.command(name="start", description="Run commands in Ubuntu 22.04")
@@ -27,14 +36,14 @@ class Bash(commands.GroupCog, name="bash"):
 		embed = discord.Embed()
 		icon = "https://d1rytvr7gmk1sx.cloudfront.net/wp-content/uploads/2021/08/tux.jpg"
 		embed.set_author(name="Ubuntu 22.04", icon_url=icon)
-		embed.add_field(name="Running ~$ `" + command + "`", value="```...```", inline=False)
+		embed.add_field(name=f"Running ~$ `{command}`", value="```...```", inline=False)
 
 		await interaction.response.send_message(embed=embed)
 
 		result = await self.run(command)
 		embed = discord.Embed()
 		embed.set_author(name="Ubuntu 22.04", icon_url=icon)
-		embed.add_field(name="Ran ~$ `" + command + "`", value="```" + result[:800] + "```", inline=False)
+		embed.add_field(name=f"Ran ~$ `{command}`", value=f"```{result[:800]}```", inline=False)
 
 		await interaction.edit_original_response(embed=embed)
 
