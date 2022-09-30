6fe662bab34573f0a13596120331b8f7586bbbd5
Andy Chen <112647105+andy12q4@users.noreply.github.com>
2022-09-30 16:08:29 -0400

    Andy - gifs update
    Added gif reactions to certain messages

--- cogs/chatbot/basic_chat - Andy.py
+++ cogs/chatbot/basic_chat - Andy.py
@@ -1,5 +1,10 @@
-# Basic Chat Response
+﻿# Basic Chat Response
 import random 
+import random 
+import io
+import aiohttp
+import discord
+
 from discord.ext import commands
 
 
@@ -8,6 +13,37 @@ class AndyChat(commands.Cog, name="Andy Chat"):
 
     def __init__(self, bot):
         self.bot = bot
+
+        
+    @commands.Cog.listener()
+
+    async def on_message(self, message):
+
+        ID = message.author.id
+
+        # gifs
+        if message.content.lower().startswith("what") and len(message.content) < 10:
+            await message.reply("https://media.giphy.com/media/zgJj5O3peaTrg95T46/giphy.gif")
+
+        elif "johncena" in message.content.lower().replace(" ", ""):
+            await message.reply("https://media.giphy.com/media/xTiTnoHt2NwerFMsCI/giphy.gif")
+
+        elif "shirley" in message.content.lower():
+            await message.reply("https://media.giphy.com/media/dHvG5FzLhmwJS3CUZj/giphy.gif")
+        
+        elif "marty" in message.content.lower():
+            await message.reply("https://media.giphy.com/media/yGoHdRNjnV7ze7LQfN/giphy.gif")
+
+        elif "trump" in message.content.lower():
+            await message.reply("https://media.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif")
+
+        elif "biden" in message.content.lower():
+            await message.reply("https://media.giphy.com/media/QWw4hc5gTnJhY0BUI3/giphy.gif")
+
+        elif "turner" in message.content.lower():
+            await message.reply("https://media.giphy.com/media/tzV14WYQwgDQ61GCvt/giphy.gif")
+            
+
 
 
 async def setup(bot):
