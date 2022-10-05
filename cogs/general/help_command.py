275999f1655dd8b97b831087b8147a827ae51298
whitefieldcat <whitefieldcat@gmail.com>
2022-10-05 19:52:55 -0400

    Revise heading text for clarity

--- cogs/general/help_command.py
+++ cogs/general/help_command.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 class HelpCommand(commands.HelpCommand):
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="All commands", description="Type `!help [cog]` for more information on any of the following cogs")
+        embed = discord.Embed(title="All commands", description="Type `!help [command]` for help with a specific command.")
 
         bot = self.context.bot.user
         embed.set_author(name=bot.name, icon_url=bot.avatar)
@@ -18,14 +18,14 @@ class HelpCommand(commands.HelpCommand):
         await self.get_destination().send(embed=embed)
 
     async def send_cog_help(self, cog):
-        embed = discord.Embed(title=f"Help for {cog.qualified_name}")
+        embed = discord.Embed(title=f"Cog `{cog.qualified_name}`")
 
         embed.description = "\n".join(f"**{command.qualified_name} {command.signature}** - {command.help}" for command in cog.get_commands())
 
         await self.get_destination().send(embed=embed)
 
     async def send_command_help(self, command):
-        embed = discord.Embed(title=f"Help for `{command.qualified_name}`")
+        embed = discord.Embed(title=f"Command `{command.qualified_name}`")
 
         embed.add_field(name="Usage", value=f"{command.qualified_name} {command.signature}", inline=False)
         embed.add_field(name="Description", value=command.help, inline=False)
