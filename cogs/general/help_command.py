a085c87ca2e439c2f8ac8444f7547db296b8bc30
Arslan <whitefieldcat@gmail.com>
2022-10-07 16:30:15 -0400

    Reword some help embed titles

--- cogs/general/help_command.py
+++ cogs/general/help_command.py
@@ -10,19 +10,19 @@ class HelpCommand(commands.HelpCommand):
                 command_names = [command.name for command in commands if command is not None]
 
                 if command_names:
-                    embed.add_field(name=cog.qualified_name, value=", ".join(command_names), inline=True)
+                    embed.add_field(name=cog.qualified_name, value=", ".join(command_names), inline=False)
 
         await self.get_destination().send(embed=embed)
 
     async def send_cog_help(self, cog):
-        embed = discord.Embed(title=f"Cog `{cog.qualified_name}`")
+        embed = discord.Embed(title=f"{cog.qualified_name}")
 
         embed.description = "\n".join(f"**{command.qualified_name} {command.signature}** - {command.help}" for command in cog.get_commands())
 
         await self.get_destination().send(embed=embed)
 
     async def send_command_help(self, command):
-        embed = discord.Embed(title=f"Command `{command.qualified_name}`")
+        embed = discord.Embed(title=f"`{command.qualified_name}`")
 
         embed.add_field(name="Usage", value=f"{command.qualified_name} {command.signature}", inline=False)
         embed.add_field(name="Description", value=command.help, inline=False)
