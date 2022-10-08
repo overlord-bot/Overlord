8936cdd64f5d0cf5b2039a49e0fe4de6397306b1
Tyler Chan <mc1231.tc@gmail.com>
2022-10-08 14:54:18 -0400

    Functioning Polls created
    Still some TODOs and more features related to displaying stats.

--- cogs/poll/util/PollInfoModal.py
+++ cogs/poll/util/PollInfoModal.py
@@ -1,6 +1,14 @@
-from code import interact
+import sys
+import time
 from cogs.poll.util.PollView import PollView
+import random
 import discord
+
+number_emojis = [ 
+	'0️⃣0️⃣', '0️⃣1️⃣', '0️⃣2️⃣', '0️⃣3️⃣', '0️⃣4️⃣', '0️⃣5️⃣', '0️⃣6️⃣', '0️⃣7️⃣', '0️⃣8️⃣', '0️⃣9️⃣', 
+	'1️⃣0️⃣', '1️⃣1️⃣', '1️⃣2️⃣', '1️⃣3️⃣', '1️⃣4️⃣', '1️⃣5️⃣', '1️⃣6️⃣', '1️⃣7️⃣', '1️⃣8️⃣', '1️⃣9️⃣',
+	'2️⃣0️⃣', '2️⃣1️⃣', '2️⃣2️⃣', '2️⃣3️⃣', '2️⃣4️⃣', '2️⃣5️⃣'
+]
 
 class PollInfoModal(discord.ui.Modal, title="Poll Information"):
 	poll_title = discord.ui.TextInput(
@@ -11,36 +19,47 @@ class PollInfoModal(discord.ui.Modal, title="Poll Information"):
 	)
 	poll_information = discord.ui.TextInput(
 		label = "Poll Information",
+		placeholder="poll option 1\npoll option 2\n...",
 		style=discord.TextStyle.long
 	)
 	poll_timeout = discord.ui.TextInput(
-		label= "Timeout",
+		label= "Timeout in minutes",
 		style=discord.TextStyle.short,
-		placeholder="How many minutes should this poll last?"
+		placeholder="How many minutes until poll times out?",
+		default="5"
 	)
-
-	# appliedcsskills.withgoogle.com
-	# g.co/techdevguide
-	# g.co/buildyourfuture
 
 	def __init__(self, title: str = None) -> None:
 		self.poll_title.default = title
 		super().__init__()
 
-	async def add_item(self) -> None:
-		item = discord.ui.TextInput(label="Item")
-		return super().add_item(item)
-
 	async def on_submit(self, interaction: discord.Interaction) -> None:
+		# TODO add error checking for non float timeouts
 		title = self.poll_title.value
 		content = self.poll_information.value.split("\n")
-		print(content)
+		send_followup = False
+		if (len(content) > 25):
+			send_followup = True
+			content = content[:25]
+
+		poll_id = f"{hex(int(time.time()))[2:]}={hex(random.randrange(0, 4294967295))[2:]}"
 		embed = discord.Embed(title=title, type="rich")
 		embed.set_author(name=interaction.user.nick, icon_url=interaction.user.display_avatar.url)
-		if (len(content) >= 25):
-			await interaction.response.send_message(ephemeral=True, content="You can't have more than 25 options")
-			return
 		for i in range(len(content)):
-			embed.add_field(name=str(i), value=content[i])
-		view = PollView(title=title, content=content, embed=embed)
-		await interaction.response.send_message(embed=embed, view=view)
+			embed.add_field(name=number_emojis[i], value=f"**{content[i]}**")
+		
+		view = PollView(
+			title=title,
+			content=content,
+			embed=embed,
+			timeout=float(self.poll_timeout.value),
+			poll_id=poll_id
+		)
+		# TODO maybe ID looks better as an embed footer?
+		await interaction.response.send_message(
+			embed=embed,
+			view=view,
+			content=f"ID:{poll_id}"
+		)
+		if send_followup:
+			await interaction.followup.send(ephemeral=True, content="You can't have more than 25 options, limiting to 25.")
--- cogs/poll/util/PollView.py
+++ cogs/poll/util/PollView.py
@@ -1,27 +1,52 @@
-from code import InteractiveConsole, interact
-from tkinter import Button
 from typing import List
 import discord
 
-numbers = [ '0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣' ]
-
 class PollView(discord.ui.View):
-	def __init__(self, title: str = None, content: List[str] = None, embed: discord.Embed = None) -> None:
+	def __init__(self, title: str, content: List[str], embed: discord.Embed, timeout:int, poll_id:str) -> None:
 		self.title = title
+		self.content = content
 		self.embed = embed
-		super().__init__()
-		for i in range(len(content)):
-			button = discord.ui.Button(style=discord.ButtonStyle.blurple, label=str(i))
-			# emoji=discord.PartialEmoji.from_str(numbers[i]))
-			button.callback = self.buttonc
+		self.num_polls = len(content)
+		self.voted = [[] for i in range(self.num_polls)]
+		self.poll_id = poll_id
+		super().__init__(timeout=60*timeout)
+
+		# Generate buttons based on how many options
+		for i in range(self.num_polls):
+			button = discord.ui.Button(
+				# TODO Change button label to display number emojis instead
+				custom_id=f"{self.poll_id}:{i}",
+				style=discord.ButtonStyle.blurple,
+			  label=str(i)
+			)
+			button.callback = self.button_callback
 			self.add_item(button)
+		
+	def on_timeout(self) -> None:
+		# TODO Clearing buttons does not work 
+		# TODO add displaying a bar chart showing stats
+		print(f"{self.title}:{self.poll_id} timed out.")
+		self.clear_items()
 
-	async def buttonc(self, interaction: discord.Interaction) -> None:
-		print(interaction.data)
-		self.embed.set_field_at(0, name="owo", value="owo2", inline=False)
+	async def button_callback(self, interaction: discord.Interaction) -> None:
+		# Check to see if the button clicked corresponds with this poll
+		id = interaction.data["custom_id"]
+		delimiter_idx = id.index(":")
+		poll_id = id[:delimiter_idx]
+		if (poll_id != self.poll_id):
+			return
+		button_id = int(id[delimiter_idx+1:])
+
+		# Add or remove the user from the option clicked
+		if interaction.user in self.voted[button_id]:
+			self.voted[button_id].remove(interaction.user)
+		else:
+			self.voted[button_id].append(interaction.user)
+
+		# Update the users who voted on this option on the embed
+		self.embed.set_field_at(
+			index=button_id,
+			name=self.embed.fields[button_id].name,
+			value=f"**{self.content[button_id]}**\n" + "\n".join(map(lambda user: user.display_name, self.voted[button_id]))
+			)
 		await interaction.response.edit_message(embed=self.embed)
-		# await interaction.edit_original_response(embed=self.embed)
-
-	# @discord.ui.button(label="Title", style=discord.ButtonStyle.blurple)
-	# async def create(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
-	# 	print(interaction)
