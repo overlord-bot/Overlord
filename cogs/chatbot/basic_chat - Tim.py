1b3efe2d0d19f07b29d1dd2388ee3b2783316a2e
timxie140 <timxie140@gmail.com>
2022-10-11 16:16:26 -0400

    add !pun & !goodbye

--- cogs/chatbot/basic_chat - Tim.py
+++ cogs/chatbot/basic_chat - Tim.py
@@ -54,10 +54,11 @@ class TimChat(commands.Cog, name="TimChat"):
          "I don't trust stairs because they're always up to something.", "My friend's bakery burned down last night. Now his business is toast.", " I wasn't originally going to get a brain transplant, but then I changed my mind.", 
          "There was a kidnapping at school yesterday. Don't worry, though - he woke up!", "What washes up on tiny beaches? Microwaves.", "Do you know how to make holy water? You boil the hell out of it.", 
          "What does my head and hell have in common? They both have demons in them", "The teacher asked, 'why are you in school on a saturday?' I told her my mum told me to go to hell.", "What do you call a monkey that loves Doritos? A chipmunk!")
-        await self.send(random.choice(JOKES))
-    
+        await message.channel.send(random.choice(JOKES))
+    @commands.command()
     async def goodbye(self, message):
-        await message.channel.send("GoodBye, " + message.author.name + ", have a great day!")
+        FAREWELL = ("See ya, ", "Bye bye, ", "See you around, ", "Alright, ", "Good to see you ")
+        await message.channel.send(random.choice(FAREWELL) + message.author.name + ", have a great day!")
         await self.bot.close()
 
 async def setup(bot):
