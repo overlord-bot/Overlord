b334ecc6383efe5386b2f6451cfe5e74086a8d07
jwgit9 <39379864+jwgit9@users.noreply.github.com>
2022-10-13 21:02:25 -0400

    Fix courses JSON file output
    - json file now contains all courses instead of just a few

--- cogs/webcrawling/rpi_catalog_scraper.py
+++ cogs/webcrawling/rpi_catalog_scraper.py
@@ -5,17 +5,19 @@ import time  # for calculating runtime
 import json
 
 
-# Finds all courses in a url based on its first child's "a" tag attributes
+# Finds all courses in an url based on its first child's "a" tag attributes
 def soup_search(url):
     search_data = requests.get(url).text
     search_soup = BeautifulSoup(search_data, 'html.parser')
-
+    
     return search_soup.find_all("a", {"style": "float:right",
                                       "href": "javascript:acalogPopup('/mime/download.?catoid=24&ftype=3&foid=', 'view_flashpoint', 770, 530, 'yes')"})
 
 
-# parses courses attributes for easier handling in the degree planner
+# parses course attributes for easier handling in the degree planner
 def get_course_info(course_list):
+    parsed_course_list = []  # list of dictionaries
+
     for course in course_list:
         course_title = course.parent.h3.text.strip()
         course_id = course_title.split(" - ")[0].strip()
@@ -64,7 +66,7 @@ def get_course_info(course_list):
         print(f"Credit Hours: {course_credit_hours}")
         '''
 
-        parsed_courses = {
+        parsed_course = {
             "course_subject": course_subject,
             "course_number": course_number,
             "course_name": course_name,
@@ -75,20 +77,22 @@ def get_course_info(course_list):
             "course_crosslisted": course_crosslisted,
             "course_credit_hours": course_credit_hours
         }
+        parsed_course_list.append(parsed_course)
 
-        return parsed_courses
+    return parsed_course_list
 
 
 # main function to generate all pages to be searched
 if __name__ == "__main__":
     start_time = time.time()
+    courses = []  # list of dictionaries
 
-    courses = {}
-
-    # link to website with all courses as of Fall 2022
+    # link to website with all courses, last updated Fall 2022
     # http://catalog.rpi.edu/content.php?filter%5B27%5D=-1&filter%5B29%5D=&filter%5Bcourse_type%5D=&filter%5Bkeyword%5D=&filter%5B32%5D=1&filter%5Bcpage%5D=1&cur_cat_oid=24&expand=1&navoid=606&print=1&filter%5Bexact_match%5D=1#acalog_template_course_filter
     subject_code = "-1"  # -1 searches for all courses, can be replaced with "CSCI" for example to specify subject
+    print("Starting Catalog Scraping")
     for page_number in range(1, 21):  # there are 20 pages in the course catalog for 2022-2023
+        print(f"Scraping Page {page_number}")
         page_url = f"http://catalog.rpi.edu/content.php?" \
                    f"filter%5B27%5D={subject_code}" \
                    f"&filter%5B29%5D=&filter%5Bcourse_type%5D=" \
@@ -96,12 +100,15 @@ if __name__ == "__main__":
                    f"&filter%5B32%5D=1&filter%5Bcpage%5D={page_number}" \
                    f"&cur_cat_oid=24&expand=1&navoid=606&print=1&filter%5Bexact_match%5D=1#acalog_template_course_filter"
 
-        course_info = get_course_info(soup_search(page_url))
-        courses[course_info["course_name"]] = course_info
+        courses.extend(get_course_info(soup_search(page_url)))  # adds list items to end of list
 
+    print("End Catalog Scraping")
+
+    print("Start writing to JSON File")
     json_object = json.dumps(courses, indent=2)
-    with open("cogs/webcrawling/catalog_results.json", "w") as outfile:  # write to file
+    with open("catalog_results.json", "w") as outfile:  # write to file
         outfile.write(json_object)
+    print("End writing to JSON File")
 
     # time to run program
     print("--- %s seconds ---" % (time.time() - start_time))
