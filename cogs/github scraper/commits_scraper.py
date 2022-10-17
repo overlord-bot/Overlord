f26b1cf77f39f3b479ae5a347127e88dc37129ed
jwgit9 <39379864+jwgit9@users.noreply.github.com>
2022-10-17 17:00:16 -0400

    Output links in txt file

--- cogs/github scraper/commits_scraper.py
+++ cogs/github scraper/commits_scraper.py
@@ -14,12 +14,13 @@ def find_links(url):
     # adds links into links_list if they are not already in it
     for link in links:
         full_link = "https://github.com" + link['href']
-        if full_link not in links_list:
-            links_list.append(full_link)
+        if full_link not in commits_list:
+            commits_list.append(full_link)
 
     # the buttons at the bottom of the page that say "Newer" and "Older"
     page_buttons = search_soup.find_all("a", {"rel": "nofollow", "class": "btn btn-outline BtnGroup-item"})
 
+    # if there are more commits on an older page, then find_links() of that older page
     has_more_pages = False
     for button in page_buttons:
         if button.get_text() == "Older":
@@ -28,11 +29,13 @@ def find_links(url):
 
 
 if __name__ == "__main__":
-    links_list = []  # list of commits in the main branch
+    commits_list = []  # list of commits in the main branch
 
     repo_url = "https://github.com/overlord-bot/Overlord/commits?author=jwgit9"
 
     find_links(repo_url)
-    for link in links_list:
-        print(link)
-    print(len(links_list))
+    username = "jwgit9"
+    with open("list_of_commits.txt", "w") as file:
+        file.write(f"{username}'s {len(commits_list)} Commits in Main Branch:\n")
+        for commit in commits_list:
+            file.write(f"{commit}\n")
