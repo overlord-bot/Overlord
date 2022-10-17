8d556e55fd7d3c7646406fe48a9c4e00c4ae7096
Andy Chen <112647105+andy12q4@users.noreply.github.com>
2022-10-17 18:54:24 -0400

    added more to functions

--- cogs/chatbot/basic_chat - Andy.py
+++ cogs/chatbot/basic_chat - Andy.py
@@ -5,7 +5,24 @@ from discord.ext import commands
 name_to_url = {"!youtube": "https://www.youtube.com/", '!yt': "https://www.youtube.com/",
                "!facebook": "https://www.facebook.com/", "!fb": "https://www.facebook.com/",
                "!reddit": "https://www.reddit.com/", "!lms": "https://lms.rpi.edu/",
-               "!quacs": "https://quacs.org/", "!sis": "https://sis.rpi.edu/"}
+               "!quacs": "https://quacs.org/", "!sis": "https://sis.rpi.edu/",
+               "!gmail": "https://mail.google.com/mail/", "!pollbuddy": "https://pollbuddy.app/",
+               "!webmail": "https://webmail.rpi.edu/", "!submitty": "https://submitty.cs.rpi.edu/home",
+               "!sheets": "https://docs.google.com/spreadsheets/", "!docs": "https://docs.google.com/document/",
+               "!google": "https://www.google.com/", "!twitter": "https://twitter.com/",
+               "!instagram": "https://www.instagram.com/", "!ig": "https://www.instagram.com/",
+               "!amazon": "https://www.amazon.com/"}
+
+gifs_react = {"johncena": "https://media.giphy.com/media/zgJj5O3peaTrg95T46/giphy.gif",
+              "marty": "https://media.giphy.com/media/yGoHdRNjnV7ze7LQfN/giphy.gif",
+              "trump": "https://media.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif",
+              "biden": "https://media.giphy.com/media/QWw4hc5gTnJhY0BUI3/giphy.gif",
+              "obama": "https://media.giphy.com/media/cEYFeDKVPTmRgIG9fmo/giphy.gif",
+              "lebron": "https://media.giphy.com/media/GS2HlaP8SzW0g/giphy.gif",
+              "turner": "https://media.giphy.com/media/tzV14WYQwgDQ61GCvt/giphy.gif",
+              "kuzman": "https://media.giphy.com/media/7PbdlEW2fyXDubicsU/giphy.gif",
+              "therock": "https://media.giphy.com/media/26ghbWoXv3G6ypo8o/giphy.gif",
+              "hillaryclinton": "https://media.giphy.com/media/l0HlE56oAxpngfnWM/giphy.gif"}
 
 
 class AndyChat(commands.Cog, name="Andy Chat"):
@@ -28,38 +45,10 @@ class AndyChat(commands.Cog, name="Andy Chat"):
             await message.reply(name_to_url[msg])
 
         # gifs
-        if message.content.lower().startswith("what") and len(message.content) < 10:
-            await message.reply("https://media.giphy.com/media/zgJj5O3peaTrg95T46/giphy.gif")
+        for key_ in gifs_react:
+            if key_ in msg.replace(' ', ''):
+                await message.reply(gifs_react[key_])
 
-        elif "johncena" in message.content.lower().replace(" ", ""):
-            await message.reply("https://media.giphy.com/media/xTiTnoHt2NwerFMsCI/giphy.gif")
-
-        elif "shirley" in message.content.lower():
-            await message.reply("https://media.giphy.com/media/dHvG5FzLhmwJS3CUZj/giphy.gif")
-
-        elif "marty" in message.content.lower():
-            await message.reply("https://media.giphy.com/media/yGoHdRNjnV7ze7LQfN/giphy.gif")
-
-        elif "trump" in message.content.lower():
-            await message.reply("https://media.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif")
-
-        elif "biden" in message.content.lower():
-            await message.reply("https://media.giphy.com/media/QWw4hc5gTnJhY0BUI3/giphy.gif")
-
-        elif "obama" in message.content.lower():
-            await message.reply("https://media.giphy.com/media/cEYFeDKVPTmRgIG9fmo/giphy.gif")
-
-        elif "lebron" in message.content.lower():
-            await message.reply("https://media.giphy.com/media/GS2HlaP8SzW0g/giphy.gif")
-
-        elif "turner" in message.content.lower():
-            await message.reply("https://media.giphy.com/media/tzV14WYQwgDQ61GCvt/giphy.gif")
-
-        elif "kuzman" in message.content.lower():
-            await message.reply("https://media.giphy.com/media/7PbdlEW2fyXDubicsU/giphy.gif")
-
-        elif "therock" in message.content.lower().replace(' ', ''):
-            await message.reply("https://media.giphy.com/media/26ghbWoXv3G6ypo8o/giphy.gif")
 
 
 async def setup(bot):
