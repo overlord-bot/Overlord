f5ea3b5d181f269794aa2fa18648af12b3cfab16
WUB8 <101521205+WUB8@users.noreply.github.com>
2022-11-08 16:12:00 -0500

    Delete some of the repeat code from my file

--- cogs/chatbot/basic_chat - Bijun.py
+++ cogs/chatbot/basic_chat - Bijun.py
@@ -7,45 +7,8 @@ from discord.ext import commands
 from datetime import datetime
 from datetime import date
 import asyncio
-
-
-class BijunChat(commands.Cog, name="BijunChat"):
-    """Basic Chatbot Functions"""
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user or message.author.bot:
-            return
-        elif message.content.lower().startswith("hi"):
-            await message.channel.send("Great " + message.author.name + " !")  # reacts with message in the location it was sent from
-        elif message.content == "msg":
-            await message.author.send('👋')  # sends a direct message to the user
-        elif message.content == "react":
-            await message.add_reaction("👍")  # adds an emoji reaction to a message, press windows key + '.' to bring up emoji list
-            await message.add_reaction("❤")
-            await message.add_reaction("🆗")
-        # not needed due to asyncio, left commented out in code in case needed later
-        # await self.bot.process_commands(message)  # https://discordpy.readthedocs.io/en/stable/faq.html#why-does-on-message-make-my-commands-stop-working
-        #additional features below
-        elif (message.content == "what the time is it?") or (message.content == "what the time is it") or (message.content == "what time") or (message.content == "time"):
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            await message.channel.send("It's " + current_time + " now!")
         
-        elif (message.content.lower() == "what day is today?") or (message.content.lower() == "what day is today") or (message.content.lower() == "what is today?") or (message.content.lower() == "date"):
-            today = date.today()
-            d = today.strftime("%B %d, %Y")
-            await message.channel.send("Today is " + d)
-
-        elif "fuck" in message.content.lower():
-            await message.delete()
-            await message.channel.send("https://c.tenor.com/fzrYWO2l7KkAAAAC/captain-america-language.gif")
-            await message.channel.send("Watch your language!")
-        
-            
+                   
 class ExtraFunc(commands.Cog, name="Additional Function "):
     def __init__(self, bot):
         self.bot = bot
@@ -163,5 +126,4 @@ class ExtraFunc(commands.Cog, name="Additional Function "):
         await ctx.send(f"Unmuted {Member.mention} for reason {reasons}")
         
 async def setup(bot):
-    await bot.add_cog(BijunChat(bot))
     await bot.add_cog(ExtraFunc(bot))
