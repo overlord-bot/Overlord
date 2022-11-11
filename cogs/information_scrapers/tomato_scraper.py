8190ee05e03cc8968af742006a4a896fb6d04598
Eagle1456 <amadeaus02@gmail.com>
2022-11-11 16:15:29 -0500

    Add documentation for arguments

--- cogs/information_scrapers/tomato_scraper.py
+++ cogs/information_scrapers/tomato_scraper.py
@@ -13,6 +13,13 @@ class RottenTomatoesScraper(commands.Cog, name="Rotten Tomatoes Scraper"):
 
     @commands.command()
     async def tomato(self, context, *args):
+        """
+        tomato <name> <type> <year>
+        Finds media from rotten tomatoes and displays it's critical score and audience score
+        <name>: Name of the media. If it has multiple words it must be in quotes!
+        <type>: Type of the media. Either can be tv or movie
+        <year>: The year that the movie came out in
+        """
         url = "https://www.rottentomatoes.com/search?search=" + "%20".join(args)
         print(f"Url of search result page: {url}")
         search_data = requests.get(url)
@@ -37,7 +44,7 @@ class RottenTomatoesScraper(commands.Cog, name="Rotten Tomatoes Scraper"):
                 audience = "N/A"
             else:
                 audience = f"{audience}%"
-            await context.send(f"Name: {name} | URL: {link} | Tomatometer: {percent} | Audience Score: {audience}")
+            await context.send(f"Name: {name}\nURL: {link}\nTomatometer: {percent}\nAudience Score: {audience}")
         else:
             await context.send("I couldn't find any results from that query. Try another one.")
 
