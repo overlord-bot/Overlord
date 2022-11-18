8e6ff09af6596a8f01fee90b7610bc14d0927fbc
VNA818-RPI <Arcerv@rpi.edu>
2022-11-18 17:22:57 -0500

    Update moderation.py

--- cogs/moderation/moderation.py
+++ cogs/moderation/moderation.py
@@ -2,7 +2,8 @@
 
 import discord
 from discord.ext import commands
-
+import os
+from datetime import datetime
 
 class Moderation(commands.Cog, name="Moderation"):
     """Startup Operations"""
@@ -26,6 +27,16 @@ class Moderation(commands.Cog, name="Moderation"):
         else:
             await interaction.response.send_message(content="Bot not running in dev mode! (No testing_server)", ephemeral=True)
     
+    @discord.app_commands.command(name="updated")
+    async def last_updates(self, interaction: discord.Interaction) -> None:
+        '''
+        Checks when bot was last updated on the server.
+        '''
+        fetch = open(os.getcwd() + '\.git\FETCH_HEAD', 'r')
+        commit_hash = fetch.readline().split('\t')[0]
+        time = os.path.getmtime((os.getcwd() + '\.git\FETCH_HEAD'))
+        time = datetime.fromtimestamp(time).strftime('%m/%d/%Y %I:%M:%S')
+        await interaction.response.send_message(content=f'Bot last updated on: {time} \nCommit Hash is: {commit_hash}', ephemeral=True)
 
 async def setup(bot: commands.bot) -> None:
     await bot.add_cog(Moderation(bot))
